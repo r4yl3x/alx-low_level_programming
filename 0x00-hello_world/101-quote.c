e63756d51e0b29708003f88f6241a3316bbd485f
raylex <raylex133@gmail.com>
2023-02-18 04:57:28 -0800

    #advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main
+ *
+ * Description: Describe
+ *
+ * Return: Always 1(Succcess)
+ * /
+
+
+int main(void)
+{
+	eprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
